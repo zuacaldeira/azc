1d2bb8b275d44326c5a955360d282c573632783b
Alexandre Caldeira <zuacaldeira@gmail.com>
2023-02-01 16:12:24 +0100

    Inital app project in IntelliJ settings

--- src/main/java/com/zuacaldeira/azc/AzcApplication.java
+++ src/main/java/com/zuacaldeira/azc/AzcApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AzcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AzcApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AzcApplication.class, args);
+    }
 
 }
