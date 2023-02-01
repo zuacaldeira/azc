c71b61afea2d5059a4b0353c1e293be97210b84e
Alexandre Caldeira <zuacaldeira@gmail.com>
2023-02-01 02:07:17 +0100

    Initial commit

---
+++ src/main/java/com/zuacaldeira/azc/AzcApplication.java
@@ -0,0 +1,13 @@
+package com.zuacaldeira.azc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzcApplication.class, args);
+	}
+
+}
